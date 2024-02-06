a01e2d41509dfe182c799fc311d6b572f6ef8f17
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-06 20:33:45 +0330

    add save method

--- src/main/java/service/VehicleService.java
+++ src/main/java/service/VehicleService.java
@@ -1,4 +1,18 @@
 package service;
 
+import model.Vehicle;
+import repository.VehicleRepository;
+
+import java.sql.SQLException;
+
 public class VehicleService {
+    private final VehicleRepository vehicleRepository ;
+
+    public VehicleService(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
+    public int save (Vehicle vehicle) throws SQLException {
+        return vehicleRepository.save(vehicle);
+    }
 }
