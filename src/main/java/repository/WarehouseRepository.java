9a1d8d150eeacfa427a2754794f6a95e64d4afdf
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-06 20:08:10 +0330

    add save method

--- src/main/java/repository/WarehouseRepository.java
+++ src/main/java/repository/WarehouseRepository.java
@@ -1,4 +1,25 @@
 package repository;
 
+import model.Warehouse;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class WarehouseRepository {
+    private final Connection connection;
+
+    public WarehouseRepository(Connection connection) {
+        this.connection = connection;
+    }
+
+    public int save (Warehouse warehouse) throws SQLException {
+        int result = 0 ;
+        String sql = "INSERT INTO warehouse (vehicle_id) VALUES (?)";
+        try (PreparedStatement ps = connection.prepareStatement(sql)){
+            ps.setInt(1,warehouse.getVehicleId());
+            result = ps.executeUpdate();
+        }
+        return result ;
+    }
 }
