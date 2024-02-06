213a2eace50399443bb65d7c2d0a213edee04514
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-06 20:24:47 +0330

    add Warehouse repository and service objects

--- src/main/java/utilities/ApplicationContext.java
+++ src/main/java/utilities/ApplicationContext.java
@@ -1,4 +1,21 @@
 package utilities;
 
+import connection.DBconnection;
+import repository.WarehouseRepository;
+import service.WarehouseService;
+
+import java.sql.Connection;
+
 public class ApplicationContext {
+
+    private final static Connection CONNECTION;
+    private final static WarehouseRepository WAREHOUSE_REPOSITORY;
+    private final static WarehouseService WAREHOUSE_SERVICE;
+
+    static {
+        CONNECTION = DBconnection.getConnection();
+        WAREHOUSE_REPOSITORY=new WarehouseRepository(CONNECTION);
+        WAREHOUSE_SERVICE=new WarehouseService(WAREHOUSE_REPOSITORY);
+    }
+    public static WarehouseService getWarehouseService(){return WAREHOUSE_SERVICE;}
 }
