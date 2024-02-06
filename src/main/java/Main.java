5d951af7e8048ec6b382100b918eefb2dbaeefa8
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-06 20:26:34 +0330

    add VehicleRepository class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
+import service.WarehouseService;
+import utilities.ApplicationContext;
+
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) throws SQLException {
+        WarehouseService warehouseService = ApplicationContext.getWarehouseService();
+        warehouseService.addWarehouse();
+    }
 }
