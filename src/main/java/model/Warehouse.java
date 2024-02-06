5c35fde6dfca647c73ef601a7ba465d30a2c9580
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-06 19:57:45 +0330

    create needed fields

--- src/main/java/model/Warehouse.java
+++ src/main/java/model/Warehouse.java
@@ -1,4 +1,15 @@
 package model;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Warehouse {
+    int id ;
+    int vehicleId ;
 }
