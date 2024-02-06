b33fb3bd0c9867cda51f14a6c6a56a2a2b88d6d8
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-06 20:01:17 +0330

    add DBconnection class

---
+++ src/main/java/connection/DBconnection.java
@@ -0,0 +1,23 @@
+package connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconnection {
+    private static java.sql.Connection connection = null;
+    public DBconnection () {}
+
+    public static java.sql.Connection getConnection()  {
+        if (connection==null){
+            String url="jdbc:postgresql://localhost:5432/hw08";
+            String username="postgres";
+            String password="lplnvqh81";
+            try {
+                connection= DriverManager.getConnection(url,username,password);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return connection;
+    }
+}
