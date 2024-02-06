db6955916184e4171249c0f2e0b4e732ce521b94
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-06 19:59:17 +0330

    add nedded fields

--- src/main/java/model/Vehicle.java
+++ src/main/java/model/Vehicle.java
@@ -1,4 +1,16 @@
 package model;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Vehicle {
+    int id ;
+    String type ;
+    int weight ;
 }
