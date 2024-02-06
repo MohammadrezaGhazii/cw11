214a4e139786394258482ebb923eb0c620cf2db5
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-06 20:35:42 +0330

    add Vehicle repository and service object

--- src/main/java/utilities/ApplicationContext.java
+++ src/main/java/utilities/ApplicationContext.java
@@ -1,7 +1,9 @@
 package utilities;
 
 import connection.DBconnection;
+import repository.VehicleRepository;
 import repository.WarehouseRepository;
+import service.VehicleService;
 import service.WarehouseService;
 
 import java.sql.Connection;
@@ -11,11 +13,17 @@ public class ApplicationContext {
     private final static Connection CONNECTION;
     private final static WarehouseRepository WAREHOUSE_REPOSITORY;
     private final static WarehouseService WAREHOUSE_SERVICE;
+    private final static VehicleRepository VEHICLE_REPOSITORY;
+    private final static VehicleService VEHICLE_SERVICE;
 
     static {
         CONNECTION = DBconnection.getConnection();
         WAREHOUSE_REPOSITORY=new WarehouseRepository(CONNECTION);
         WAREHOUSE_SERVICE=new WarehouseService(WAREHOUSE_REPOSITORY);
+
+        VEHICLE_REPOSITORY=new VehicleRepository(CONNECTION);
+        VEHICLE_SERVICE=new VehicleService(VEHICLE_REPOSITORY);
     }
     public static WarehouseService getWarehouseService(){return WAREHOUSE_SERVICE;}
+    public static VehicleService getVehicleService(){return VEHICLE_SERVICE;}
 }
