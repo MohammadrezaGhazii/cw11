5dd5d99b8d9b5381e2290e9931633907319db6ca
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-06 20:30:45 +0330

    add save method

--- src/main/java/repository/VehicleRepository.java
+++ src/main/java/repository/VehicleRepository.java
@@ -1,4 +1,27 @@
 package repository;
 
+import model.Vehicle;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class VehicleRepository {
+
+    private final Connection connection;
+
+    public VehicleRepository(Connection connection) {
+        this.connection = connection;
+    }
+    public int save (Vehicle vehicle) throws SQLException {
+        int result = 0 ;
+        String sql = "INSERT INTO vehicle (type, weight) VALUES (?,?)";
+        try (PreparedStatement ps = connection.prepareStatement(sql)){
+            ps.setString(1, vehicle.getType());
+            ps.setInt(2,vehicle.getWeight());
+
+            result = ps.executeUpdate();
+        }
+        return result;
+    }
 }
